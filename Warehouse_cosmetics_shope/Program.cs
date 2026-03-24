1547599cc4e785563e5c5bd6887555b674e12691
arisha <arishaurazova2007@gmail.com>
2026-03-24 21:06:46 +0500

    Удаление лишней формы

--- Warehouse_cosmetics_shope/Program.cs
+++ Warehouse_cosmetics_shope/Program.cs
@@ -14,9 +14,6 @@ namespace Warehouse_cosmetics_shope
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
         }
     }
 }
