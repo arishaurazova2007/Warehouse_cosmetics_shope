8268dadc66b7d605a93f5e89dc936a69df090944
arisha <arishaurazova2007@gmail.com>
2026-03-26 22:14:05 +0500

    добавила логгер, try catch и хеширование

--- Warehouse_cosmetics_shope/DataBaseClass/WarehouseContext.cs
+++ Warehouse_cosmetics_shope/DataBaseClass/WarehouseContext.cs
@@ -39,31 +39,5 @@ namespace Warehouse_cosmetics_shope.DataBaseClass
         /// Детализированный состав каждой отгрузки
         /// </summary>
         public DbSet<ShipmentComposition> ShipmentCompositions { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            // Настройка самоссылающейся связи для категорий
-            modelBuilder.Entity<Category>()
-                .HasOptional(c => c.Parent)
-                .WithMany(c => c.SubCategories)
-                .HasForeignKey(c => c.ParentID)
-                .WillCascadeOnDelete(false); // Отключаем каскад, чтобы не удалить всё дерево целиком
-
-            // Настройка связи Состава с Отгрузкой
-            modelBuilder.Entity<ShipmentComposition>()
-                .HasRequired(sc => sc.Shipment)
-                .WithMany(s => s.ShipmentCompositions)
-                .HasForeignKey(sc => sc.ShipmentID)
-                .WillCascadeOnDelete(true); // При удалении отгрузки удаляем её строки
-
-            // Настройка связи Состава с Товаром
-            modelBuilder.Entity<ShipmentComposition>()
-                .HasRequired(sc => sc.Product)
-                .WithMany(p => p.ShipmentCompositions)
-                .HasForeignKey(sc => sc.ProductID)
-                .WillCascadeOnDelete(false); // Не удаляем товар при удалении отгрузки!
-
-            base.OnModelCreating(modelBuilder);
-        }
     }
 }
--- Warehouse_cosmetics_shope/Program.cs
+++ Warehouse_cosmetics_shope/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Serilog;
 using Warehouse_cosmetics_shope.DataBaseClass;
 
 namespace Warehouse_cosmetics_shope
@@ -12,103 +13,116 @@ namespace Warehouse_cosmetics_shope
         [STAThread]
         static void Main()
         {
-            using (WarehouseContext db = new WarehouseContext())
+            Log.Logger = new LoggerConfiguration().WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+            try
             {
-                db.Items.RemoveRange(db.Items);
-                db.Categories.RemoveRange(db.Categories);
-                db.Users.RemoveRange(db.Users);
-                db.SaveChanges();
-
-                db.Users.AddRange(new List<User>
+                Log.Information("Приложение запущено");
+                using (WarehouseContext db = new WarehouseContext())
                 {
-                    new User
-                    { 
-                        UserID = Guid.NewGuid(),
-                        Surname = "Иванова",
-                        Name = "Татьяна",
-                        Patronymic = "Константиновна",
-                        Password = "16Kl7SDt!",
-                        Role = Roles.Админ
-                    },
-                    new User 
+                    db.Items.RemoveRange(db.Items);
+                    db.Categories.RemoveRange(db.Categories);
+                    db.Users.RemoveRange(db.Users);
+                    db.SaveChanges();
+
+                    db.Users.AddRange(new List<User>
                     {
-                        UserID = Guid.NewGuid(),
-                        Surname = "Кузнецов",
-                        Name = "Антон",
-                        Patronymic = "Юрьевич",
-                        Password = "Hy56E2))L3",
-                        Role = Roles.Админ
-                    },
-                    new User
+                        new User
+                        {
+                            UserID = Guid.NewGuid(),
+                            Surname = "Иванова",
+                            Name = "Татьяна",
+                            Patronymic = "Константиновна",
+                            Password = BCrypt.Net.BCrypt.HashPassword("16Kl7SDt!"),// Хешируем!
+                            Role = Roles.Админ
+                        },
+                        new User
+                        {
+                            UserID = Guid.NewGuid(),
+                            Surname = "Кузнецов",
+                            Name = "Антон",
+                            Patronymic = "Юрьевич",
+                            Password = BCrypt.Net.BCrypt.HashPassword("Hy56E2))L3"),
+                            Role = Roles.Админ
+                        },
+                        new User
+                        {
+                            UserID = Guid.NewGuid(),
+                            Surname = "Смирнов",
+                            Name = "Андрей",
+                            Patronymic = "Павлович",
+                            Password = BCrypt.Net.BCrypt.HashPassword("8d2Tb%Q245"),
+                            Role = Roles.Админ
+                        }
+                    });
+
+                    var catPerfume = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Парфюмерия" };
+                    var catCosmetics = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Косметика" };
+                    db.Categories.AddRange(new List<Category> { catPerfume, catCosmetics });
+
+                    var catFemale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Женская парфюмерия", ParentID = catPerfume.CategoryID };
+                    var catMale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Мужская парфюмерия", ParentID = catPerfume.CategoryID };
+                    var catDecor = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Декоративная", ParentID = catCosmetics.CategoryID };
+                    var catCare = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Уходовая", ParentID = catCosmetics.CategoryID };
+                    db.Categories.AddRange(new List<Category> { catFemale, catMale, catDecor, catCare });
+
+                    var subCatDuhyFemale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Духи женские", ParentID = catFemale.CategoryID };
+                    var subCatDuhyMale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Духи мужские", ParentID = catMale.CategoryID };
+                    var subCatWaterPerfumFemale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Парфюмерная вода женская", ParentID = catFemale.CategoryID };
+                    var subCatWaterPerfumMale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Парфюмерная вода мужская", ParentID = catMale.CategoryID };
+                    var subCatWaterFemale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Туалетная вода женская", ParentID = catFemale.CategoryID };
+                    var subCatWaterMale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Туалетная вода мужская", ParentID = catMale.CategoryID };
+                    db.Categories.AddRange(new List<Category> { subCatDuhyFemale, subCatDuhyMale, subCatWaterFemale, subCatWaterMale, subCatWaterPerfumFemale, subCatWaterPerfumMale });
+
+                    db.SaveChanges();
+
+                    db.Items.AddRange(new List<Item>
                     {
-                        UserID = Guid.NewGuid(),
-                        Surname = "Смирнов",
-                        Name = "Андрей",
-                        Patronymic = "Павлович",
-                        Password = "8d2Tb%Q245",
-                        Role = Roles.Админ
-                    }
-                });
+                        new Item
+                        {
+                            ProductID = Guid.NewGuid(),
+                            ProductName = "Chanel №5 (50 мл)",
+                            CategoryID = catFemale.CategoryID, // Привязка к созданной категории
+                            Price = 12100,
+                            Quantity = 14,
+                            Units = MeasurementUnits.Шт,
+                            ExpDate = new DateTime(2028, 09, 15)
+                        },
+                        new Item
+                        {
+                            ProductID = Guid.NewGuid(),
+                            ProductName = "Librederm Гиалуроновый крем, 50 мл",
+                            CategoryID = catCare.CategoryID,
+                            Price = 810,
+                            Quantity = 145,
+                            Units = MeasurementUnits.Шт,
+                            ExpDate = new DateTime(2028, 03, 16)
+                        },
+                        new Item
+                        {
+                            ProductID = Guid.NewGuid(),
+                            ProductName = "KRYGINA Блеск для губ LIP MAXIMALIST",
+                            CategoryID = catDecor.CategoryID,
+                            Price = 1050,
+                            Quantity = 57,
+                            Units = MeasurementUnits.Шт,
+                            ExpDate = new DateTime(2028, 05, 18)
+                        }
+                    });
 
-                var catPerfume = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Парфюмерия" };
-                var catCosmetics = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Косметика" };
-                db.Categories.AddRange(new List<Category> { catPerfume, catCosmetics });
-
-                var catFemale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Женская парфюмерия", ParentID = catPerfume.CategoryID };
-                var catMale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Мужская парфюмерия", ParentID = catPerfume.CategoryID };
-                var catDecor = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Декоративная", ParentID = catCosmetics.CategoryID };
-                var catCare = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Уходовая", ParentID = catCosmetics.CategoryID };
-                db.Categories.AddRange(new List<Category> { catFemale, catMale, catDecor, catCare });
-
-                var subCatDuhyFemale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Духи женские", ParentID = catFemale.CategoryID };
-                var subCatDuhyMale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Духи мужские", ParentID = catMale.CategoryID };
-                var subCatWaterPerfumFemale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Парфюмерная вода женская", ParentID = catFemale.CategoryID };
-                var subCatWaterPerfumMale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Парфюмерная вода мужская", ParentID = catMale.CategoryID };
-                var subCatWaterFemale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Туалетная вода женская", ParentID = catFemale.CategoryID };
-                var subCatWaterMale = new Category { CategoryID = Guid.NewGuid(), CategoryName = "Туалетная вода мужская", ParentID = catMale.CategoryID };
-
-                db.Categories.AddRange(new List<Category> { subCatDuhyFemale, subCatDuhyMale, subCatWaterFemale, subCatWaterMale });
-
-                db.SaveChanges();
-
-                db.Items.AddRange(new List<Item>
-                {
-                    new Item
-                    {
-                        ProductID = Guid.NewGuid(),
-                        ProductName = "Chanel №5 (50 мл)",
-                        CategoryID = catFemale.CategoryID, // Привязка к созданной категории
-                        Price = 12100,
-                        Quantity = 14,
-                        Units = MeasurementUnits.Шт,
-                        ExpDate = new DateTime(2028, 09, 15)
-                    },
-                    new Item
-                    {
-                        ProductID = Guid.NewGuid(),
-                        ProductName = "Librederm Гиалуроновый крем, 50 мл",
-                        CategoryID = catCare.CategoryID,
-                        Price = 810,
-                        Quantity = 145,
-                        Units = MeasurementUnits.Шт,
-                        ExpDate = new DateTime(2028, 03, 16)
-                    },
-                    new Item
-                    {
-                        ProductID = Guid.NewGuid(),
-                        ProductName = "KRYGINA Блеск для губ LIP MAXIMALIST",
-                        CategoryID = catDecor.CategoryID,
-                        Price = 1050,
-                        Quantity = 57,
-                        Units = MeasurementUnits.Шт, 
-                        ExpDate = new DateTime(2028, 05, 18)
-                    }
-                });
-
-                db.SaveChanges();
-                Console.WriteLine("База данных успешно заполнена данными из таблицы!");
+                    db.SaveChanges();
+                    Console.WriteLine("База данных успешно заполнена данными из таблицы!");
+                }
             }
-            Console.ReadLine();
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Произошла ошибка!");
+            }
+            finally
+            {
+                Log.CloseAndFlush(); // Сохраняем логи перед выходом
+                if (!Console.IsInputRedirected)
+                    Console.ReadKey();
+            }
         }
     } 
 }
