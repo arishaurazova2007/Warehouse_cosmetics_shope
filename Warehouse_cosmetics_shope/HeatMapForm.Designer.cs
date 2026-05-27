09f92df6c7d1aeb18b7813ab8b93cdd5d1c09b2e
Maria <mashasozinova1@gmail.com>
2026-05-27 14:05:13 +0300

    Добавлена форма тепловой карты склада: отображение товаров по ячейкам с цветовой индикацией срока годности и остатка

---
+++ Warehouse_cosmetics_shope/HeatMapForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Warehouse_cosmetics_shope
+{
+    partial class HeatMapForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.legendPanel = new System.Windows.Forms.Panel();
+            this.mapPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(618, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Тепловая карта склада";
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.buttonBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBack.Location = new System.Drawing.Point(993, 12);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(105, 41);
+            this.buttonBack.TabIndex = 1;
+            this.buttonBack.Text = "Назад";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // legendPanel
+            // 
+            this.legendPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(238)))), ((int)(((byte)(242)))));
+            this.legendPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.legendPanel.Location = new System.Drawing.Point(0, 0);
+            this.legendPanel.Name = "legendPanel";
+            this.legendPanel.Size = new System.Drawing.Size(349, 589);
+            this.legendPanel.TabIndex = 2;
+            // 
+            // mapPanel
+            // 
+            this.mapPanel.AutoScroll = true;
+            this.mapPanel.BackColor = System.Drawing.Color.White;
+            this.mapPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mapPanel.Location = new System.Drawing.Point(386, 75);
+            this.mapPanel.Name = "mapPanel";
+            this.mapPanel.Size = new System.Drawing.Size(724, 502);
+            this.mapPanel.TabIndex = 3;
+            // 
+            // HeatMapForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(238)))), ((int)(((byte)(242)))));
+            this.ClientSize = new System.Drawing.Size(1122, 589);
+            this.Controls.Add(this.mapPanel);
+            this.Controls.Add(this.legendPanel);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.label1);
+            this.Name = "HeatMapForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Тепловая карта склада";
+            this.Load += new System.EventHandler(this.HeatMapForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Panel legendPanel;
+        private System.Windows.Forms.Panel mapPanel;
+    }
+}
