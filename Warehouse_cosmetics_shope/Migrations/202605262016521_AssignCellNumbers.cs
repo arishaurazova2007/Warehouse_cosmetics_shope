e0f8a98e504162ade56771aa404acee2a4f60f39
Maria <mashasozinova1@gmail.com>
2026-05-27 14:04:25 +0300

    Добавлена миграция AssignCellNumbers( присвоение номеров ячеек существующим товарам)

---
+++ Warehouse_cosmetics_shope/Migrations/202605262016521_AssignCellNumbers.cs
@@ -0,0 +1,27 @@
+﻿namespace Warehouse_cosmetics_shope.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class AssignCellNumbers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"
+                WITH numbered AS (
+                    SELECT ProductID, 
+                           ROW_NUMBER() OVER (ORDER BY ProductNumber) AS rn
+                    FROM Items 
+                    WHERE CellNumber = 0
+                )
+                UPDATE Items 
+                SET CellNumber = numbered.rn
+                FROM numbered 
+                WHERE Items.ProductID = numbered.ProductID
+            ");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
